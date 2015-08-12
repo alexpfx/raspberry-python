9f4eb994032d934fd992213457ea6f57a552aa67
alexpfx <alexpfba@gmail.com>
2015-08-12 20:00:10 -0300

    uarever

---
+++ network/sockets.py
@@ -0,0 +1,14 @@
+__author__ = 'alexandre'
+import socket
+
+print("creating socket")
+s = socket.socket()
+print("done")
+
+port = socket.getservbyname('http', 'tcp')
+
+s.connect(("www.uol.com", port))
+
+print("Connected from: ",  s.getsockname())
+print("Connected to: ", s.getpeername())
+
